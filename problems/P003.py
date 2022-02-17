9340329923ca774cdf7e21973328f8a7cc9b2bf2
Metee Yingyongwatthanakit <metee.yingyong@kkumail.com>
2022-02-17 23:48:23 +0700

    complete P03

---
+++ problems/P003.py
@@ -0,0 +1,31 @@
+import math
+
+if __name__ == '__main__':
+    a, b, c, d = [int(i) for i in input().split()]
+    if a == 1:
+        c, d = d, c  # swap value for c and d
+        if b == 1:
+            c += d
+        elif b == 2:
+            c -= d
+        elif b == 3:
+            c = c + (c ** d)
+        else:
+            c /= d
+        a = (d + math.sqrt((c / b) ** 3 + d)) / (2 + b * d)
+    elif a == 2:
+        if b > 1:
+            c += d
+        if b > 2:
+            c /= d
+        if b > 3:
+            c = c + (c ** d)
+        else:
+            a = math.log10(c)
+    else:
+        while a > d:
+            a -= 2
+            if a < b:
+                break
+            c += a
+    print(a, b, c, d)
