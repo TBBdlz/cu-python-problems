a27f7d5261fed9982f358898e2ad1de955332e4f
Metee Yingyongwatthanakit <metee.yingyong@kkumail.com>
2022-02-20 21:52:35 +0700

    complete P05.py aprroximate pi value

---
+++ problems/P005.py
@@ -0,0 +1,39 @@
+import math
+
+c: str = input()
+if c == 'S':
+    m: int = int(input())
+    q, r, t, k, n, x, i = 1, 0, 1, 1, 3, 3, 0
+    p: str = ''
+    while i < m:
+        if 4 * q + r - t < n * t:
+            p += str(n)
+            i += 1
+            a = 10 * (r - n * t)
+            n = 10 * (3 * q + r) // t - 10 * n
+            q *= 10
+        else:
+            a = (2 * q + r) * x
+            b = (7 * q * k + 2 + x * r) // (x * t)
+            q *= k
+            t *= x
+            x += 2
+            k += 1
+            n = b
+        r = a
+    p = f'{p[0]}.{p[1:]}'
+    print(f'pi = {p}')
+elif c == 'R':
+    n: int = int(input())
+    p: float = 0
+    for k in range(n + 1):
+        p += math.pow(-3, -k) / (2 * k + 1)
+    p *= math.sqrt(12)
+    p = round(p, 12)
+    print(f'pi = {p}')
+elif c == 'P':
+    p = math.sqrt(7 + math.sqrt(6 + math.sqrt(5)))
+    p = round(p, 6)
+    print(f'pi = {p}')
+else:
+    print("Invalid")
