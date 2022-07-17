4f6b531028c9bba0d7a6b36eee19d239b614c7c3
Metee <metee.yingyong@kkumail.com>
2022-07-17 13:28:44 +0700

    improve readability on P4-6

--- contents/repetition/P04_06.py
+++ contents/repetition/P04_06.py
@@ -1,15 +1,24 @@
-expression: str = input()
+"""
+	P04-06: swap bracket notation
+"""
 
-for char in expression:
-    if char == '(':
-        print('[', end='')
-    elif char == ')':
-        print(']', end='')
 
-    elif char == '[':
-        print('(', end='')
-    elif char == ']':
-        print(')', end='')
-    else:
-        print(char, end='')
-print()
+def swap_bracket(sarg: str) -> None:
+    for char in expression:
+        if char == '(':
+            print('[', end='')
+        elif char == ')':
+            print(']', end='')
+
+        elif char == '[':
+            print('(', end='')
+        elif char == ']':
+            print(')', end='')
+        else:
+            print(char, end='')
+    print()  # print new line character
+
+
+if __name__ == '__main__':
+    expression: str = input()
+    swap_bracket(expression)
