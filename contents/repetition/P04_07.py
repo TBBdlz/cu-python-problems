9d7526374e29c36f2c7298e93e39fa068a74ef06
Metee Yingyongwatthanakit <metee.yingyong@kkumail.com>
2022-07-18 21:54:26 +0700

    improve readability

--- contents/repetition/P04_07.py
+++ contents/repetition/P04_07.py
@@ -2,14 +2,12 @@
 	P04-07: Interested word counter
 """
 
+
 keyword: str = input()
 sentence: str = input()
 first_char = keyword[0]
-num_word: int = 0
+num_word: int = sum(sentence[i: i + len(
+    keyword)] == keyword for i in range(len(sentence)))
 
-for i in range(len(sentence)):
-    if first_char == sentence[i]:
-        if sentence[i:i+len(keyword)] == keyword:
-            num_word += 1
 
 print(num_word)
