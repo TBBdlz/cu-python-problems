4e1324928aa574f221139a8025e414e13324473f
Metee <metee.yingyong@kkumail.com>
2022-07-25 03:41:26 +0700

    Simplify code

--- contents/repetition/P04_10.py
+++ contents/repetition/P04_10.py
@@ -5,27 +5,14 @@
 
 
 enc_data: str = input()
-encoding_pair: list = []
 
 temp = enc_data[0]
-counter = 0
-for i in range(len(enc_data)):
-	if i == len(enc_data) - 1:
-		if temp == enc_data[i]:
-			counter += 1
-			encoding_pair.append((enc_data[i], counter))
-		else:
-			encoding_pair.extend(((temp, counter), (enc_data[i], 1)))
-		continue
-
-	if enc_data[i] == temp:
+counter = 1
+for char in enc_data[1:]:
+	if char == temp:
 		counter += 1
 	else:
-		encoding_pair.append((temp, counter))
-		temp = enc_data[i]
+		print(temp, counter, end=' ')
+		temp = char
 		counter = 1
-
-for pair in encoding_pair:
-	print(f'{pair[0]} {pair[1]}', end='')
-	print(' ', end='')
-print()
+print(temp, counter)
