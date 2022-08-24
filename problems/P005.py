331383f6332c684ee0559fb6643feb18fffd3fef
Metee Yingyongwatthanakit <metee.yingyong@kkumail.com>
2022-08-24 19:34:18 +0700

    improve readability

--- problems/P005.py
+++ problems/P005.py
@@ -1,7 +1,17 @@
 import math
 
 c: str = input()
-if c == 'S':
+if c == 'P':
+    p = math.sqrt(7 + math.sqrt(6 + math.sqrt(5)))
+    p = round(p, 6)
+    print(f'pi = {p}')
+elif c == 'R':
+    n: int = int(input())
+    p: float = sum(math.pow(-3, -k) / (2 * k + 1) for k in range(n + 1))
+    p *= math.sqrt(12)
+    p = round(p, 12)
+    print(f'pi = {p}')
+elif c == 'S':
     m: int = int(input())
     q, r, t, k, n, x, i = 1, 0, 1, 1, 3, 3, 0
     p: str = ''
@@ -22,18 +32,6 @@ if c == 'S':
             n = b
         r = a
     p = f'{p[0]}.{p[1:]}'
-    print(f'pi = {p}')
-elif c == 'R':
-    n: int = int(input())
-    p: float = 0
-    for k in range(n + 1):
-        p += math.pow(-3, -k) / (2 * k + 1)
-    p *= math.sqrt(12)
-    p = round(p, 12)
-    print(f'pi = {p}')
-elif c == 'P':
-    p = math.sqrt(7 + math.sqrt(6 + math.sqrt(5)))
-    p = round(p, 6)
     print(f'pi = {p}')
 else:
     print("Invalid")
